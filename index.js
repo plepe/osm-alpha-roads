ab31a0c812447bfd1b3bbd1f62c3ca33984c3d3f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-02-03 20:40:37 +0100

    Find all nodes with roads with names with consecutive 1st letter

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const fs = require('fs')
 const buildNodesIndex = require('./src/buildNodesIndex')
 const buildNamesIndex = require('./src/buildNamesIndex')
 const findCrossNodes = require('./src/findCrossNodes')
+const findCrossNodesAB = require('./src/findCrossNodesAB')
 
 let data = JSON.parse(fs.readFileSync('data.json'))
 
 let nodesIndex = buildNodesIndex(data)
 let namesIndex = buildNamesIndex(data)
 let crossNodes = findCrossNodes(nodesIndex)
+let crossNodesAB = findCrossNodesAB(crossNodes)
 
-console.log(crossNodes)
+console.log(crossNodesAB)
---
+++ src/findCrossNodesAB.js
@@ -0,0 +1,33 @@
+module.exports = function findCrossNodesAB (data) {
+  let result = {}
+
+  for (let nodeId in data) {
+    let node = data[nodeId]
+
+    let found = false
+    node.forEach(
+      (a, ai) => {
+        let codeA = a.codePointAt(0)
+
+        return node.forEach(
+          (b, bi) => {
+            if (ai === bi) {
+              return codeA
+            }
+
+            let codeB = b.codePointAt(0)
+            if (codeA == codeB + 1 || codeA == codeB - 1) {
+              found = true
+            }
+          }
+        )
+      }
+    )
+
+    if (found) {
+      result[nodeId] = node
+    }
+  }
+
+  return result
+}
